253a2d853e2e33f6feada4d1416a9db25267b4a1
Mahmud Lukman <mistamann49@gmail.com>
2024-01-25 15:47:22 +0100

    register server action added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { LoginButton } from '@/components/auth/LoginButton';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { Poppins } from 'next/font/google';
+import Image from 'next/image';
 
 const font = Poppins({
   subsets: ['latin'],
@@ -12,14 +13,17 @@ export default function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-black to-slate-800">
       <div className="space-y-6 text-center">
-        <h1
-          className={cn(
-            'text-6xl font-semibold text-white drop-shadow-md',
-            font.className
-          )}
-        >
-          Socials
-        </h1>
+        <div className="flex gap-4">
+          <Image src="/logo.svg" alt="logo" width={68} height={68} />
+          <h1
+            className={cn(
+              'text-6xl font-semibold text-white drop-shadow-md',
+              font.className
+            )}
+          >
+            Socials
+          </h1>
+        </div>
         <p className="text-white text-lg">A The best social media app</p>
         <div>
           <LoginButton>
